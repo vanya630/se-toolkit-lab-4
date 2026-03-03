78fa52ac3793631cf4d40905e5432535fd382144
Danila Danko <br4ch1st0chr0n3@gmail.com>
2026-03-03 11:53:08 +0300

    fix(backend): make all env variables required

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -5,24 +5,24 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     # Environment variables
 
-    app_name: str = Field(default="Learning Management Service", alias="NAME")
-    debug: bool = Field(default=False, alias="DEBUG")
-    address: str = Field(default="127.0.0.1", alias="ADDRESS")
-    port: int = Field(default=8000, alias="PORT")
-    reload: bool = Field(default=False, alias="RELOAD")
+    app_name: str = Field(..., alias="NAME")
+    debug: bool = Field(..., alias="DEBUG")
+    address: str = Field(..., alias="ADDRESS")
+    port: int = Field(..., alias="PORT")
+    reload: bool = Field(..., alias="RELOAD")
 
-    api_key: str = Field(alias="API_KEY")
+    api_key: str = Field(..., alias="API_KEY")
 
     cors_origins: list[str] = Field(default=[], alias="CORS_ORIGINS")
 
-    enable_interactions: bool = Field(default=False, alias="APP_ENABLE_INTERACTIONS")
-    enable_learners: bool = Field(default=False, alias="APP_ENABLE_LEARNERS")
+    enable_interactions: bool = Field(..., alias="APP_ENABLE_INTERACTIONS")
+    enable_learners: bool = Field(..., alias="APP_ENABLE_LEARNERS")
 
-    db_host: str = Field(default="localhost", alias="DB_HOST")
-    db_port: int = Field(default=5432, alias="DB_PORT")
-    db_name: str = Field(default="lab-4", alias="DB_NAME")
-    db_user: str = Field(default="postgres", alias="DB_USER")
-    db_password: str = Field(default="postgres", alias="DB_PASSWORD")
+    db_host: str = Field(..., alias="DB_HOST")
+    db_port: int = Field(..., alias="DB_PORT")
+    db_name: str = Field(..., alias="DB_NAME")
+    db_user: str = Field(..., alias="DB_USER")
+    db_password: str = Field(..., alias="DB_PASSWORD")
 
     model_config = SettingsConfigDict(
         env_file_encoding="utf-8",
